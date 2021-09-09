a183f16856116e364579f750b916507598a19316
FrankieRG <garciaf90@gmail.com>
2021-09-08 18:00:48 -0700

    auth and blog complete

---
+++ controllers/auth.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.js');
+const SECRET = process.env.SECRET;
+
+
+// hashing function
+const hash = (password) => {
+  const levelOne = crypto.createHmac('sha256', process.env.SECRET)
+                  .update(password)
+                  .digest('hex')
+                  .split('')
+                  .reverse()
+                  .join('j')
+  return crypto.createHmac('sha256', process.env.SECRET)
+                  .update(levelOne)
+                  .digest('hex')
+                  .split('')
+                  .reverse()
+                  .join('')
+}
+
+module.exports.hash = hash
+
+const registerService = async (req, res) => {
+  const hashedPassword = hash(req.body.password)
+  console.log('hashedPassword:', hashedPassword )
+  req.body.password = bcrypt.hashSync(hashedPassword, bcrypt.genSaltSync(10))
+  console.log(req.body)
+
+  try{
+    const createdUser = await User.create(req.body)
+    const token = jwt.sign({
+      username: createdUser.username
+    }, SECRET)
+    res.status(200).json({ user: createdUser, token })
+  }catch(err){
+    console.error(err)
+    res.status(400).json({ msg: err.message })
+  }
+}
+
+module.exports.register = registerService
+
+// verification
+
+// login
+
+const loginService = async (req, res) => {
+  try {
+    const foundUser = await User.findOne({ username: req.body.username })
+    req.body.password = hash(req.body.password)
+    if(bcrypt.compareSync(req.body.password, foundUser.password)){
+      const token = jwt.sign({
+        username: foundUser.username
+      }, SECRET)
+      res.status(200).json({ user: foundUser, token })
+    }else {
+      throw new Error('Authentication failed! Try again')
+    }
+  }catch(err){
+    console.error(err)
+    res.status(400).json({ msg: err.message })
+  }
+}
+
+module.exports.login = loginService
---
+++ server.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const NoeticTalk = ('./models/noetic')
+const userController = require('./controllers/users');
+const blogsController = require('./controllers/blogs');
+const { hash, register, login } = require('./controllers/auth');
+const MONGODB_URI = process.env.MONGODB_URI;
+const db = mongoose.connection;
+
+app.get('/', function (req, res) {
+    res.send('Welcome to Noetic Talk');
+});
+
+// Database
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+db.on('open', () => {
+    console.log('Mongo is Connected');
+});
+
+
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use((req, res, next) => {
+    console.log('**********************')
+    console.log('***********Middleware checking in***********')
+    console.log('I run before all routes')
+    console.log('**********************')
+    next()
+  })
+
+  app.use(express.urlencoded({ extended: true }))
+
+
+
+app.use(express.json());
+if (process.env.NODE_ENV !== 'development'){
+  app.use(express.static('public'))
+};
+
+app.use('/api/blogs', blogsController);
+app.post('/register', register);
+app.post('/login', login);
+
+
+
+
+
+
+
+
+// ...Other code
+// ...More code
+app.listen(PORT,()=>{
+    console.log('WE OUTSIDE', PORT);
+});
